cc6a0f1bab9e8488dd62c25220865a853373a772
abiyasa05 <abiyasap24@gmail.com>
2025-04-01 11:24:34 +0700

    create: add validation modal for assessment

---
+++ Penilaian Literasi/iClOP-V2/resources/views/literacy/student/assessments/modals/unanswered_warning.blade.php
@@ -0,0 +1,18 @@
+<!-- Modal Peringatan Soal Belum Dijawab -->
+<div class="modal fade" id="unansweredWarningModal" tabindex="-1" role="dialog">
+    <div class="modal-dialog">
+        <div class="modal-content">
+            <div class="modal-header">
+                <h5 class="modal-title" id="unansweredWarningModalLabel">Soal Belum Terjawab</h5>
+                <button type="button" class="btn-close" data-dismiss="modal" aria-label="Close"></button>
+            </div>
+            <div class="modal-body">
+                <p>Anda masih memiliki <strong><span id="unansweredCount"></span></strong> soal yang belum dijawab.</p>
+                <p>Silakan selesaikan terlebih dahulu sebelum mengumpulkan asesmen.</p>
+            </div>
+            <div class="modal-footer">
+                <button type="button" class="btn btn-secondary" data-dismiss="modal">Kembali</button>
+            </div>
+        </div>
+    </div>
+</div>
