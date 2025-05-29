cfd6261fd4bfe6cbb09f5d39caa01397ae60486f
abiyasa05 <abiyasap24@gmail.com>
2025-05-29 09:08:09 +0700

    update: add column and relations for model literacy generated text

--- Penilaian Literasi/iClOP-V2/app/Models/Literacy/LiteracyGeneratedText.php
+++ Penilaian Literasi/iClOP-V2/app/Models/Literacy/LiteracyGeneratedText.php
@@ -13,12 +13,21 @@ class LiteracyGeneratedText extends Model
 
     protected $fillable = [
         'user_id',
+        'material_id',
         'generate_text',
+        'question_type',
+        'question_count',
+        'created_at',
+        'updated_at'
     ];
 
     public function teacher()
     {
         return $this->belongsTo(\App\Models\User::class, 'user_id');
     }
-    
+
+    public function material()
+    {
+        return $this->belongsTo(\App\Models\Literacy\LiteracyMaterial::class, 'material_id');
+    }
 }
